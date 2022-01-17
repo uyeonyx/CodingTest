acc4cda0ac1c22c20b5eba1a388859d31b9aeab0
uyeonyx <uyeonyx@gmail.com>
2022-01-17 19:07:30 +0900

    auto update

--- Programmers/test/Solution.py
+++
@@ -1,9 +0,0 @@
-def solution(arr):
-    a=[]
-    b=-1
-    for i in range(len(arr)):
-        if i > 0:
-            b=arr[i-1]
-        if arr[i] is not b:
-            a.append(arr[i])
-    return a
